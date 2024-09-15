8ffe0418eb33e452229147193a6d7c4e1d316edd
Eduardo Alvarez <el_alienx@hotmail.com>
2024-09-15 08:19:46 +0200

    adding state images

--- src/cosmos/components/EmptyState.fixture.jsx
+++ src/cosmos/components/EmptyState.fixture.jsx
@@ -5,31 +5,31 @@ import EmptyState from "../../components/empty-state/EmptyState";
 const home = {
   title: "No products available",
   text: "We are riding at full speed to add products to our store as soon as possible!",
-  image: "",
+  image: "state-home.avif",
   alt: "A guy ridding a red Vespa motorcycle. He does not realize that is dropping his pizza behind.",
 };
 const product1 = {
   title: "Invalid product",
   text: "This product does not exist. Maybe you just saw it in a dream!",
-  image: "",
+  image: "state-product-1.avif",
   alt: "A guy sleping in a bed shaped like a dollar bill.",
 };
 const product2 = {
   title: "Product not available",
-  text: "",
-  image: "",
-  alt: "",
+  text: "This product is temporally sold out. We are running to our warehouse to get more!",
+  image: "state-product-2.avif",
+  alt: "A guy running desperately almost spilling his coffe.",
 };
 const checkout = {
   title: "Your Cart is empty",
   text: "Please go back to the home page and add some products. Capitalism needs you!",
-  image: "",
+  image: "state-checkout.avif",
   alt: "A girl inside a supermarket shopping cart listening to music using her phone.",
 };
 const page404 = {
   title: "Page not found",
   text: "We did our best but could not find your page. Check the link and try again!",
-  image: "",
+  image: "state-page-404.avif",
   alt: "A guy checking his mobile phone. A screnshot of his mobile with a map is shown behind.",
 };
 
