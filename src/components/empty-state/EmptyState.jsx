b1c906d720b6b1f95a4a2a7bb65cb3a06f7f4780
Eduardo Alvarez <el_alienx@hotmail.com>
2024-09-15 07:57:53 +0200

    adding more text states

--- src/components/empty-state/EmptyState.jsx
+++ src/components/empty-state/EmptyState.jsx
@@ -5,17 +5,21 @@ import PropTypes from "prop-types";
 import "./empty-state.css";
 
 EmptyState.propTypes = {
-  /** A clear title explaining what happened. */
-  title: PropTypes.string.isRequired,
-  /** Add some humor to further explain the situation. */
-  text: PropTypes.string.isRequired,
-  /** An image related to the text above. */
-  image: PropTypes.string.isRequired,
-  /** Description of the image for accessibility. It's optional as it won't break the componet, but please add it. */
-  alt: PropTypes.string,
+  item: PropTypes.exact({
+    /** A clear title explaining what happened. */
+    title: PropTypes.string.isRequired,
+    /** Add some humor to further explain the situation. */
+    text: PropTypes.string.isRequired,
+    /** An image related to the text above. */
+    image: PropTypes.string.isRequired,
+    /** Description of the image for accessibility. It's optional as it won't break the componet, but please add it. */
+    alt: PropTypes.string,
+  }),
 };
 
-export default function EmptyState(title, text, image, alt) {
+export default function EmptyState({ item }) {
+  const { title, text, image, alt } = item;
+
   return (
     <div className="empty-state">
       <img src={image} alt={alt} />
--- src/cosmos/components/EmptyState.fixture.jsx
+++ src/cosmos/components/EmptyState.fixture.jsx
@@ -1,13 +1,42 @@
 // Project files
 import EmptyState from "../../components/empty-state/EmptyState";
 
+// Properties
+const home = {
+  title: "No products available",
+  text: "We are riding at full speed to add products to our store as soon as possible!",
+  image: "",
+  alt: "A guy ridding a red Vespa motorcycle. He does not realize that is dropping his pizza behind.",
+};
+const product1 = {
+  title: "Invalid product",
+  text: "This product does not exist. Maybe you just saw it in a dream!",
+  image: "",
+  alt: "A guy sleping in a bed shaped like a dollar bill.",
+};
+const product2 = {
+  title: "Product not available",
+  text: "",
+  image: "",
+  alt: "",
+};
+const checkout = {
+  title: "Your Cart is empty",
+  text: "Please go back to the home page and add some products. Capitalism needs you!",
+  image: "",
+  alt: "A girl inside a supermarket shopping cart listening to music using her phone.",
+};
+const page404 = {
+  title: "Page not found",
+  text: "We did our best but could not find your page. Check the link and try again!",
+  image: "",
+  alt: "A guy checking his mobile phone. A screnshot of his mobile with a map is shown behind.",
+};
+
 export default {
-  Normal: (
-    <EmptyState
-      title="No products available"
-      text="We are riding at full speed to add products to our store as soon as possible!"
-      image=""
-      alt="A guy ridding a red Vespa motorcycle. He does not realize that is dropping his pizza behind."
-    />
-  ),
+  HomeNoProducts: <EmptyState item={home} />,
+  ProductInvalidId: <EmptyState item={product1} />,
+  ProductNotAvailable: <EmptyState item={product2} />,
+  CheckoutEmpty: <EmptyState item={checkout} />,
+  Page404: <EmptyState item={page404} />,
 };
