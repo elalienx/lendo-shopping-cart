b203c8bda6a5759f100ce26269ffe449c3efbfa4
Eduardo Alvarez <el_alienx@hotmail.com>
2024-09-15 07:48:47 +0200

    adding text to states

--- src/cosmos/components/EmptyState.fixture.jsx
+++ src/cosmos/components/EmptyState.fixture.jsx
@@ -1,1 +1,13 @@
 // Project files
+import EmptyState from "../../components/empty-state/EmptyState";
+
+export default {
+  Normal: (
+    <EmptyState
+      title="No products available"
+      text="We are riding at full speed to add products to our store as soon as possible!"
+      image=""
+      alt="A guy ridding a red Vespa motorcycle. He does not realize that is dropping his pizza behind."
+    />
+  ),
+};
