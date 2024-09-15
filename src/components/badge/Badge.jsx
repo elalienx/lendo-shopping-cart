078a9a559207517ca46ab52e5c5cc69c65832694
Eduardo Alvarez <el_alienx@hotmail.com>
2024-09-15 09:58:34 +0200

    starting to add badge

---
+++ src/components/badge/Badge.jsx
@@ -0,0 +1,14 @@
+// Node modules
+import PropTypes from "prop-types";
+
+// Project files
+import "./badge.css";
+
+Badge.propTypes = {
+  /** A number to display in this notification badge. */
+  number: PropTypes.string.isRequired,
+};
+
+export default function Badge({ number }) {
+  return <div className="badge">{number}</div>;
+}
