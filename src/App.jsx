e32472d9976e8ddea2d0ed87996c8ead3fd93a56
Eduardo Alvarez <el_alienx@hotmail.com>
2024-09-15 07:14:14 +0200

    added pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@
 import { Route, Routes } from "react-router-dom";
 
 // Project files
+import Checkout from "./pages/checkout/Checkout";
+import Home from "./pages/home/Home";
+import Page404 from "./pages/page-404/Page404";
+import Product from "./pages/product/Product";
 
 export default function App() {
   // Properties
-  const home = <Home />;
-  const product = <Product />;
   const checkout = <Checkout />;
+  const home = <Home />;
   const pageNotFound = <Page404 />;
+  const product = <Product />;
 
   return (
     <div className="app">
