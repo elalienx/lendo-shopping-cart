88a7c3b4206ec286033748b341ecb865d0086c16
Eduardo Alvarez <el_alienx@hotmail.com>
2024-09-15 23:24:24 +0200

    adding css

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,6 +1,8 @@
 // Project files
+import Button from "../../components/button/Button";
 import EmptyState from "../../components/empty-state/EmptyState";
 import EmptyStateText from "./empty-state-text.json";
+import PriceTag from "../../components/price-tag/PriceTag";
 import { useCart } from "../../state/CartContext";
 import "./checkout.css";
 
@@ -13,8 +15,16 @@ export default function Checkout() {
 
   return (
     <div id="checkout" className="page">
-      <h1>Your Cart</h1>
-      <small>{cart.length} items in cart</small>
+      <div className="content-group">
+        <h1>Your Cart</h1>
+        <small>{cart.length} items in cart</small>
+        <section className="grid">{/* CheckoutItems go here */}</section>
+        <section className="grand-total">
+          <span className="label">Grand Total:</span>
+          <PriceTag price={0} />
+        </section>
+        <Button label="Proceed to payment" icon="cash-register" onClick={() => alert("End of demo 🎉")} />
+      </div>
     </div>
   );
 }
