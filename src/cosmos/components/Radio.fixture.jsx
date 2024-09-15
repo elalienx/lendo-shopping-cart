550ee0470e2d06b9577cd4ac620bf50d0123482b
Eduardo Alvarez <el_alienx@hotmail.com>
2024-09-16 00:39:33 +0200

    fix Radio fixture

--- src/cosmos/components/Radio.fixture.jsx
+++ src/cosmos/components/Radio.fixture.jsx
@@ -3,9 +3,9 @@ import Radio from "../../components/input-radio/Radio";
 
 // Properties
 const id = "color";
-const selectedIndex = 1; // to enable the blue item
+const fakeState = [1, () => {}];
 
 export default {
-  Default: <Radio id={id} name="Red" index={0} selectedIndex={selectedIndex} />,
-  Active: <Radio id={id} name="Blue" index={1} selectedIndex={selectedIndex} />,
+  Default: <Radio id={id} index={0} state={fakeState} name="Red" />,
+  Active: <Radio id={id} index={1} state={fakeState} name="Blue" />,
 };
