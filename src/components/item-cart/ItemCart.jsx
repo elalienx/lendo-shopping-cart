1b310fcaa5362282607bc5feeeb9ea430c30a02c
Eduardo Alvarez <el_alienx@hotmail.com>
2024-09-17 20:27:18 +0200

    added propTypes to ItemCart

--- src/components/item-cart/ItemCart.jsx
+++ src/components/item-cart/ItemCart.jsx
@@ -1,12 +1,40 @@
 // Node modules
+import PropTypes from "prop-types";
 import toast from "react-hot-toast";
 
 // Project files
+import Product from "../../propTypes/Product";
 import ImageThumbnail from "../image-thumbnail/ImageThumbnail";
 import NotAvailable from "../not-available/NotAvailable";
 import ButtonCircle from "../button-circle/ButtonCircle";
 import PriceTag from "../price-tag/PriceTag";
 import "./item-cart.css";
+
+ItemCart.propTypes = {
+  /** A specific product matching the cart item */
+  product: Product.item.isRequired,
+
+  /** The cart item containing the product id, color, variant, and quantity selected. */
+  item: PropTypes.shape({
+    /** The id of the product in the inventory.json used to brind the product description and price. */
+    id: PropTypes.number.isRequired,
+
+    /** The color of a product as as the index to know what variant to use. */
+    color: PropTypes.number.isRequired,
+
+    /** Specific variant of a prodcut like 250 or 500 GB of storage. */
+    variant: PropTypes.number.isRequired,
+
+    /** The amount of units the user decided to purchase. */
+    quantity: PropTypes.number.isRequired,
+  }).isRequired,
+
+  /** The position inside the cart array, used for the dispatcher. */
+  index: PropTypes.number.isRequired,
+
+  /** The actions of the cart global state. */
+  dispatch: PropTypes.func.isRequired,
+};
 
 export default function ItemCart({ product, item, index, dispatch }) {
   const { color, quantity } = item;
